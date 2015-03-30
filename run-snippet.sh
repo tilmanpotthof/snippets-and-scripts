49c95e6afdbd97ecadaab0ea5411d3d45de9d12e
Tilman Potthof <tilmanpotthof@googlemail.com>
2015-03-30 07:16:21 +0200

    added script to execute other snippet scripts globally

---
+++ run-snippet.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+export BASE_CMD=$1
+
+CONFIG_PATH=$HOME'/.snippetrc'
+source $CONFIG_PATH
+
+if [ -z $SNIPPETS_PATH ]; then
+	echo 'No config found at '$CONFIG_PATH
+    exit 1
+fi
+
+export RUN_SNIPPETS_VERSION=0.1.0
+
+EXPECTED_SCRIPT_PATH=$SNIPPETS_PATH/$BASE_CMD.sh
+
+
+if [ -z $1 ] || [ ! -f $EXPECTED_SCRIPT_PATH ]; then
+	eval echo "\"$(cat run-snippet.txt)\""
+    exit 1
+fi
+
+$EXPECTED_SCRIPT_PATH $2 $3 $4 $5
+
+# install: ln -s /Users/tpotthof/Development/snippets-and-scripts/run-snippet.sh /usr/local/bin/run-snippet
